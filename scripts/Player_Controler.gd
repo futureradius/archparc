01c31df90022f5b983067828ab717096cf741147
futureradius <futureradius@gmail.com>
2025-04-11 16:28:55 +0200

    Redid FPS Controler and added webdeploy

---
+++ scripts/Player_Controler.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+
+const SPEED = 3.0
+const JUMP_VELOCITY = 3.50
+const gravtiy = 9.8
+var mouse_sensitivity = 0.0025
+
+#head bob properties
+var headbob_frequency = 3.4
+var heafbob_amplitude = 0.5#0.04
+var headbob_side_to_side_strength = .5
+var headbob_time = 0.0
+
+@onready var head = $"Head Pivot"
+@onready var camera = $"Head Pivot/Camera3D"
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _physics_process(delta) -> void:
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = 0
+			velocity.z = 0
+	
+	
+		
+	#Add headbob to the camera
+	headbob_time = headbob_time + (delta * velocity.length() * float(is_on_floor()))
+	camera.transform.origin = headbob(headbob_time)
+	
+	move_and_slide()
+
+func headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * headbob_frequency) * heafbob_amplitude
+	#Side to side headbob: freq / 2 so that every second "step" is on the left, alternating from l to r
+	pos.x = cos(time * headbob_frequency / 2) * heafbob_amplitude * headbob_side_to_side_strength
+	return pos
