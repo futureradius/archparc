8f27402566a25a2565fb9e124713fe50d675d9f5
futureradius <futureradius@gmail.com>
2025-04-11 20:46:16 +0200

    Import of Demo Scene
    Import of Barcelona Pavillion as a demo scene, updated webdeploy

--- scripts/Player_Controler.gd
+++ scripts/Player_Controler.gd
@@ -1,14 +1,16 @@
 extends CharacterBody3D
 
 
-const SPEED = 3.0
+const WALK_speed = 3.0
+const SLOW_WALK_speed = 1.0
 const JUMP_VELOCITY = 3.50
 const gravtiy = 9.8
+var speed = 3.0
 var mouse_sensitivity = 0.0025
 
 #head bob properties
 var headbob_frequency = 3.4
-var heafbob_amplitude = 0.5#0.04
+var headbob_amplitude = 0.04
 var headbob_side_to_side_strength = .5
 var headbob_time = 0.0
 
@@ -34,6 +36,12 @@ func _physics_process(delta) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	#Handle Sprint
+	if Input.is_action_pressed("sprint"):
+		speed = SLOW_WALK_speed
+	else:
+		speed = WALK_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -42,8 +50,8 @@ func _physics_process(delta) -> void:
 	
 	if is_on_floor():
 		if direction:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
 		else:
 			velocity.x = 0
 			velocity.z = 0
@@ -58,7 +66,7 @@ func _physics_process(delta) -> void:
 
 func headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
-	pos.y = sin(time * headbob_frequency) * heafbob_amplitude
+	pos.y = sin(time * headbob_frequency) * headbob_amplitude
 	#Side to side headbob: freq / 2 so that every second "step" is on the left, alternating from l to r
-	pos.x = cos(time * headbob_frequency / 2) * heafbob_amplitude * headbob_side_to_side_strength
+	pos.x = cos(time * headbob_frequency / 2) * headbob_amplitude * headbob_side_to_side_strength
 	return pos
